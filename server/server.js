d5ca3b8beeea50eb884c3e3c47c28af66a5c7aa1
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-12-09 17:44:00 -0800

    Greet new user, and alert others.

--- server/server.js
+++ server/server.js
@@ -18,14 +18,31 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('new user connected');
+    
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'A new user has joined the chat',
+        createdAt: new Date().getTime()
+    })
 
     socket.on('createMessage', (message) => {
         console.log('create message:', message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            cretaedAt: new Date().getTime()
-        })
+            createdAt: new Date().getTime()
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
