37da75e183884f623ea7f2b2c01e44a3d65df6ef
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-12-10 22:47:57 -0800

    add form form messages and show incoming messages in browser.

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,27 @@
 var socket = io();   //initiates request. makes request from client to server to open up web socket and keep the socket open
 
+
 socket .on('connect', function() {
     console.log('connected to server');
 });
 
 socket.on('newMessage', function(message) {
     console.log('Received new message', message);
+    var li = $('<li>').text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
 });
+
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function() {
+
+    });
+})
