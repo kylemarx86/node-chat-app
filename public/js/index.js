0e433f9443e120bdbf583c6197436d5be4d4c834
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-12-11 16:35:09 -0800

    add autoscrolling when messages user is near the bottom.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();   //initiates request. makes request from client to server to open up web socket and keep the socket open
 
+function scrollToBottom() {
+    // Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
 
 socket .on('connect', function() {
     console.log('connected to server');
@@ -18,6 +33,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -30,12 +46,7 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime,
     });
     $('#messages').append(html);
-
-
-    // var $li = $('<li>').text(`${message.from} ${formattedTime}: `);
-    // var $a = $('<a>').attr({'target':'_blank', 'href':`${message.url}`}).text('My current location');
-    // $li.append($a);
-    // $('#messages').append($li);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {
